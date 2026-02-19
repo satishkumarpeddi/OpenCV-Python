c73eb9098e027e0be262e8f2ec28be89aa89a21c
evhook <anonymous1986222@gmail.com>
2026-02-19 10:20:51 +0530

    Add input/output functionality with example images in inputOutput.py

---
+++ InputOutput/inputOutput.py
@@ -0,0 +1,7 @@
+import os
+import cv2
+#read image
+image_path = os.path.join('.','InputOutput','some_image.png')
+img = cv2.imread(image_path)
+#write image
+cv2.imwrite(os.path.join('.','InputOutput','some_image_out.png'),img)
