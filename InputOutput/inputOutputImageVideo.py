628fb633a98c6980d67a82eff6934b8459a2a5bc
evhook <anonymous1986222@gmail.com>
2026-02-19 10:45:43 +0530

    Add input/output functionality for images and videos in inputOutputImage.py and inputOutputImageVideo.py

---
+++ InputOutput/inputOutputImageVideo.py
@@ -0,0 +1,14 @@
+import os 
+import cv2
+#read video
+video_path = os.path.join('.','InputOutput','SampleVideoForOpenCV.mp4')
+video = cv2.VideoCapture(video_path)
+#visualize video
+ret = True
+while ret:
+    ret,frame = video.read()
+    if ret:
+        cv2.imshow('frame',frame)
+        cv2.waitKey(40)
+video.release()
+cv2.destroyAllWindows()
