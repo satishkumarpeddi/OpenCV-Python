c6697eef35a5aac1e8b70da9eca59b27134de51a
evhook <anonymous1986222@gmail.com>
2026-02-19 10:23:36 +0530

    Add image visualization functionality in inputOutput.py

--- InputOutput/inputOutput.py
+++ InputOutput/inputOutput.py
@@ -5,3 +5,6 @@ image_path = os.path.join('.','InputOutput','some_image.png')
 img = cv2.imread(image_path)
 #write image
 cv2.imwrite(os.path.join('.','InputOutput','some_image_out.png'),img)
+#visualize image
+cv2.imshow('image',img)
+cv2.waitKey(0)
